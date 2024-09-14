90e899a858d7d30b182f1a03798d3268eefa89f4
Johan Quimbayo <johansa2394@gmail.com>
2024-09-14 10:59:43 -0500

    Cambio Archivos

--- src/componentes/Banner.jsx
+++ src/componentes/Banner.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Banner() {
     return (
         <div className="banner">
+            
             <h2>Kredilibranza</h2>
         </div>
     );
