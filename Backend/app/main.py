eac3a2d5eb2b13142927687b15421f55b6442d4b
Johan Quimbayo <johansa2394@gmail.com>
2024-11-12 22:01:14 -0500

    Ajuste routers

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,28 +1,19 @@
-# Backend/app/main.py
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routers.rag_router import rag_router  # Importa la instancia de APIRouter
+from app.api.routers import rag_router  
 
 app = FastAPI()
 
-# Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",  # Para desarrollo local
-    
-        "https://sprightly-dasik-62446f.netlify.app",
-    ],
+    allow_origins=["http://localhost:3000","https://sprightly-dasik-62446f.netlify.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Incluir el router con el prefijo "/api"
-app.include_router(rag_router, prefix="/api")
+app.include_router(rag_router)
 
-# Punto de entrada para desarrollo local
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
